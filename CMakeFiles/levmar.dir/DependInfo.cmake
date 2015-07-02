0e1628dc7885f0fd117b7ed9b741fc7779c9eb8f
Ben <speedyswimmer1000@gmail.com>
2015-07-02 09:58:07 -0600

    First commit

---
+++ CMakeFiles/levmar.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/benjamin/Dropbox/Research/Code/levmar-2.6/Axb.c" "/home/benjamin/Dropbox/Research/Code/levmar-2.6/CMakeFiles/levmar.dir/Axb.c.o"
+  "/home/benjamin/Dropbox/Research/Code/levmar-2.6/lm.c" "/home/benjamin/Dropbox/Research/Code/levmar-2.6/CMakeFiles/levmar.dir/lm.c.o"
+  "/home/benjamin/Dropbox/Research/Code/levmar-2.6/lmbc.c" "/home/benjamin/Dropbox/Research/Code/levmar-2.6/CMakeFiles/levmar.dir/lmbc.c.o"
+  "/home/benjamin/Dropbox/Research/Code/levmar-2.6/lmblec.c" "/home/benjamin/Dropbox/Research/Code/levmar-2.6/CMakeFiles/levmar.dir/lmblec.c.o"
+  "/home/benjamin/Dropbox/Research/Code/levmar-2.6/lmbleic.c" "/home/benjamin/Dropbox/Research/Code/levmar-2.6/CMakeFiles/levmar.dir/lmbleic.c.o"
+  "/home/benjamin/Dropbox/Research/Code/levmar-2.6/lmlec.c" "/home/benjamin/Dropbox/Research/Code/levmar-2.6/CMakeFiles/levmar.dir/lmlec.c.o"
+  "/home/benjamin/Dropbox/Research/Code/levmar-2.6/misc.c" "/home/benjamin/Dropbox/Research/Code/levmar-2.6/CMakeFiles/levmar.dir/misc.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
